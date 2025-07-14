542573e840a7e7ea897a75586fbc5c5e0d502492
Ankur Bhowmik <bhowmikankur07112002@gmail.com>
2025-07-14 16:44:42 +0530

    create routes

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,12 +1,18 @@
-const asyncHandler = (fn) => async(req, res, next) => {
-    try {
-        await fn(req, res, next);
-    } catch (error) {
-        res.status(error.code || 500).json({
-            message: error.message || 'Internal Server Error',      
-            success: false
-        });
-    }
+// const asyncHandler = (fn) => async(req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             message: error.message || 'Internal Server Error',      
+//             success: false
+//         });
+//     }
+// }
+
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => {next(error);})}
 }
 
 export {asyncHandler};
